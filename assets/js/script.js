dd1c10c9996123e688c376d4c2108f3b24a2ecf3
Rodrigosan003 <r.santanaa003@gmail.com>
2026-05-14 10:34:40 -0300

    landingpageSalao

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,12 @@
 let links = document.querySelectorAll(".nav-bar a");
 let menu = document.querySelector("#menu");
 
-
 links.forEach((link) => {
-    link.addEventListener("click", () => {
-        
-        // remove de todos
-        links.forEach((l) => l.classList.remove("active"));
+  link.addEventListener("click", () => {
+    // remove de todos
+    links.forEach((l) => l.classList.remove("active"));
 
-        // adiciona no clicado
-        link.classList.add("active");
-    });
+    // adiciona no clicado
+    link.classList.add("active");
+  });
 });
-
