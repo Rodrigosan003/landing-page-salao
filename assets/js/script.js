1d0efaff32215587d50da091e8e305ee842c3d2c
Rodrigosan003 <r.santanaa003@gmail.com>
2026-05-15 10:43:18 -0300

    landingpageSalao

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 let links = document.querySelectorAll(".nav-bar a");
 let sections = document.querySelectorAll(".section");
+let navLinks = document.querySelectorAll("header nav a");
+
+
 let menu = document.querySelector("#menu");
 
 links.forEach((link) => {
@@ -11,3 +14,28 @@ links.forEach((link) => {
     link.classList.add("active");
   });
 });
+
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  menuIcon.classList.remove("bi-x");
+  navbar.classList.remove("active");
+};
